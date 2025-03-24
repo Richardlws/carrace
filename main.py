bcba9cc28d4de9cd8321d419d3e124df31337d14
Richardlws <liu.bing66066@hotmail.com>
2025-03-25 06:31:31 +0800

    normal modify

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 import time
 import math
+from utils import scale_image
 
-GRASS = pygame.image.load("imgs/grass.jpg")
-TRACK = pygame.image.load("imgs/track.png")
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
-TRACK_BORDER = pygame.image.load("imgs/track-border.png")
+TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 FINISH = pygame.image.load("imgs/finish.png")
 
-RED_CAR = pygame.image.load("imgs/red-car.png")
-GREEN_CAR = pygame.image.load("imgs/green-car.png")
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing Game!")
+FPS = 60
 
+run = True
+clock = pygame.time.Clock()
+
+while run:
+    clock.tick(FPS)
+
+    WIN.blit(GRASS, (0, 0))
+    WIN.blit(TRACK, (0, 0))
+    WIN.blit(GREEN_CAR, (0, 0))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+pygame.quit()
