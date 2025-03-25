62c9e9582fd5bbcf65599260a24b6c012fb11b29
Richardlws <liu.bing66066@hotmail.com>
2025-03-26 06:47:59 +0800

    norma modify

--- main.py
+++ main.py
@@ -15,17 +15,41 @@ GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
+
 FPS = 60
+
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+
+
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
+
 
 run = True
 clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
 
 while run:
     clock.tick(FPS)
 
-    WIN.blit(GRASS, (0, 0))
-    WIN.blit(TRACK, (0, 0))
-    WIN.blit(GREEN_CAR, (0, 0))
+    draw(WIN, images)
+
+    #WIN.blit(GRASS, (0, 0))
+    #WIN.blit(TRACK, (0, 0))
+    #WIN.blit(GREEN_CAR, (0, 0))
     pygame.display.update()
 
     for event in pygame.event.get():
