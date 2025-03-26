74049abca6ac04a1ea5800d58aa9528f54e76e50
Richardlws <liu.bing66066@hotmail.com>
2025-03-27 07:05:21 +0800

    norma modify

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from utils import scale_image
+from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
@@ -19,7 +19,10 @@ pygame.display.set_caption("Racing Game!")
 FPS = 60
 
 class AbstractCar:
+    IMG = RED_CAR
+
     def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
         self.rotation_vel = rotation_vel
@@ -31,26 +34,33 @@ class AbstractCar:
         elif right:
             self.angle -= self.rotation_vel
 
+    def draw(self, win):
+        blit_rotate_center(win, self.img)
 
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
 
-def draw(win, images):
+def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos)
+
+    player_car.draw()
+    pygame.display.update()
 
 
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+player_car = PlayerCar(4, 4)
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN, images)
+    draw(WIN, images, player_car)
 
     #WIN.blit(GRASS, (0, 0))
     #WIN.blit(TRACK, (0, 0))
     #WIN.blit(GREEN_CAR, (0, 0))
-    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
